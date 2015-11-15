7dc94263b8ae446fc9fdae8b1a38be5ba4e7c232
Mikhail <m.s.sokolov.92@gmail.com>
2015-11-15 20:05:18 +0300

    refactor

--- Editor/main.cs
+++ Editor/main.cs
@@ -11,29 +11,29 @@ namespace Editor
         static void Main(string[] args)
         {
             // 4
-            IFigure dc4 = new Circle(new Point(70, 70), 30);
-            IFigure dr4 = new Rectangle(new Point(-40, 40), new Point(-90, 40), new Point(-90, 90), new Point(-40, 90));
-            IFigure dt4 = new Triangle(new Point(-20, -40), new Point(20, -40), new Point(0, 0));
+            //IFigure dc4 = new Circle(new Point(70, 70), 30);
+            //IFigure dr4 = new Rectangle(new Point(-40, 40), new Point(-90, 40), new Point(-90, 90), new Point(-40, 90));
+            //IFigure dt4 = new Triangle(new Point(-20, -40), new Point(20, -40), new Point(0, 0));
 
-            dc4 = new BorderDecorator(dc4);
-            dt4 = new BorderDecorator(dt4);
-            dt4 = new RemoveLastPropertyDecorator(dt4);
+            //dc4 = new BorderDecorator(dc4);
+            //dt4 = new BorderDecorator(dt4);
+            //dt4 = new RemoveLastPropertyDecorator(dt4);
 
-            CompositeFigure root4 = new CompositeFigure();
-            root4.Add(dt4);
-            root4.Add(dr4);
-            root4.Add(dc4);
+            //CompositeFigure root4 = new CompositeFigure();
+            //root4.Add(dt4);
+            //root4.Add(dr4);
+            //root4.Add(dc4);
 
-            IFigure forClone = new ShadowDecorator(root4);
-            forClone = new ShadowDecorator(forClone);
-            forClone = new BorderDecorator(forClone);
-            //forClone = new RemoveLastPropertyDecorator(forClone);
+            //IFigure forClone = new ShadowDecorator(root4);
+            //forClone = new ShadowDecorator(forClone);
+            //forClone = new BorderDecorator(forClone);
+            ////forClone = new RemoveLastPropertyDecorator(forClone);
 
-            IFigure IFSD4 = forClone.Clone();
-            forClone = root4;
+            //IFigure IFSD4 = forClone.Clone();
+            //forClone = root4;
 
-            IFSD4.SetShower(new ConsoleShower());
-            IFSD4.Show();
+            //IFSD4.SetShower(new ConsoleShower());
+            //IFSD4.Show();
 
             //IFSD.SetShower(new WindowShower(0, 200));
             //IFSD.Show();
@@ -49,10 +49,10 @@ namespace Editor
             IFigure dr5 = new Rectangle(new Point(-40, 40), new Point(-90, 40), new Point(-90, 90), new Point(-40, 90));
             IFigure dt5 = new Triangle(new Point(-20, -40), new Point(20, -40), new Point(0, 0));
             CompositeFigure root5 = new CompositeFigure();
-            root4.Add(dc5);
-            root4.Add(dr5);
-            root4.Add(dt5);
-            root4.MoveOn(new Point(-5, 5));
+            root5.Add(dc5);
+            root5.Add(dr5);
+            root5.Add(dt5);
+            root5.MoveOn(new Point(-5, 5));
 
             Registry r1 = Registry.GetInstance();
             r1.Add(dc5);    // [0]
