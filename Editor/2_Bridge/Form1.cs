3ef6f111025413f86fac4939e0b723200f3518fa
Mikhail <m.s.sokolov.92@gmail.com>
2015-11-15 21:05:57 +0300

    Без костылей

--- Editor/2_Bridge/Form1.cs
+++ Editor/2_Bridge/Form1.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace Editor
 {
-    partial class Form1 : Form
+    public partial class Form1 : Form
     {
         public Bitmap bmp = new Bitmap(500, 500);
         public Graphics graphics;
